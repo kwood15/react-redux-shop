7fcf4dd475fc8764b527ef64a2d471dceee08844
Kay Wood <11405108+kwood15@users.noreply.github.com>
2019-02-19 08:59:35 +0000

    Add ProductList component

--- src/App.tsx
+++ src/App.tsx
@@ -1,113 +1,21 @@
-import React, { Component } from 'react';
+import React, { FC } from 'react';
 import { connect } from 'react-redux';
 import axios from 'axios';
 // import {  } from '../actions/';
+import ProductList from './components/Product';
 import './scss/main.scss';
 
-type Book = {
-  records: {
-    data: {
-      key: string;
-      url?: string;
-      title: string;
-      subtitle: string;
-      cover: {
-        small?: string;
-      };
-      authors: Author[];
-      publish_date: string;
-    };
-  };
-};
-
-type Author = {
-  key: string;
-  name?: string;
-  url?: string;
-};
-
-type State = {
-  books: Book[];
-};
-
-class App extends Component<{}, State> {
-  readonly state: State = {
-    books: []
-  };
-
-  // todo remove from state
-  componentDidMount() {
-    this.saveBooks();
-  }
-
-  async getBooks() {
-    const response = await axios.get('/api/books');
-    const body = await response.data;
-    if (response.status !== 200) throw new Error(body.message);
-    return body;
-  }
-
-  saveBooks() {
-    this.getBooks()
-      .then(response => {
-        this.setState({
-          books: response
-        });
-      })
-      .catch(error => console.log(error));
-  }
-
-  render() {
-    const { books } = this.state;
-    return (
-      <div className="app">
-        <header />
-        <main>
-          <section>
-            {books.map(book => (
-              <div key={book.records.data.key}>
-                <h3>
-                  {book.records.data.title} {book.records.data.subtitle}
-                </h3>
-                <a
-                  href={book.records.data.url}
-                  target="_blank"
-                  title={
-                    book.records.data.subtitle
-                      ? `${book.records.data.title} ${book.records.data.subtitle}`
-                      : book.records.data.title
-                  }
-                >
-                  <img
-                    src={book.records.data.cover.small}
-                    alt={book.records.data.title}
-                  />
-                </a>
-                <p>Published: {book.records.data.publish_date}</p>
-                <p>Author details</p>
-                <p>
-                  {book.records.data.authors.map(author => (
-                    <span key={author.name}>{author.name}</span>
-                  ))}
-                </p>
-                <a
-                  href={`${book.records.data.authors.map(
-                    author => author.url
-                  )}`}
-                  target="_blank"
-                  title="View author details"
-                >
-                  View author details >>
-                </a>
-              </div>
-            ))}
-          </section>
-        </main>
-        <footer />
-      </div>
-    );
-  }
-}
+const App: FC = () => (
+  <div className="app">
+    <header />
+    <main>
+      <section>
+        <ProductList />
+      </section>
+    </main>
+    <footer />
+  </div>
+);
 
 export default App;
 // export default connect(
